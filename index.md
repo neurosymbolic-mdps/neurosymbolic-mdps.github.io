2d82c5dc2df4f2ff39ec11bdcc3ee40727e3f2d1
Cam Allen <camallen@berkeley.edu>
2025-01-30 11:08:43 -0800

    Add basic website

---
+++ index.md
@@ -0,0 +1,12 @@
+---
+title: Skill-Driven Neurosymbolic State Abstractions
+layout: default
+---
+
+
+&nbsp;
+
+## Media
+
+{: align="center"}
+![image](images/pforward-s2dp.png)
---
+++ js/hideshowelement.js
@@ -0,0 +1,8 @@
+function HideShowElement(elementId) {
+    var x = document.getElementById(elementId);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
